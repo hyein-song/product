7cd1c8ccfcb74d42c82dd10ac51abc875d52ed87
hyein-song <shi9512@naver.com>
2022-06-04 22:43:39 +0900

    service 초기화

---
+++ src/main/java/com/ssg/product/service/PromotionService.java
@@ -0,0 +1,9 @@
+package com.ssg.product.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PromotionService {
+}
