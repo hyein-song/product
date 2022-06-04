664b8cb705efb4ddde9bf60e0556e338df36be1a
hyein-song <shi9512@naver.com>
2022-06-04 22:41:56 +0900

    controller 초기화

---
+++ src/main/java/com/ssg/product/controller/PromotionController.java
@@ -0,0 +1,11 @@
+package com.ssg.product.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PromotionController {
+}
---
+++ src/main/java/com/ssg/product/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.ssg.product.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+}
